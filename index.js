c8e12d6691d315db4b11a0960c4bee2566704791
Mark Skrypka <mark.skrypka@gmail.com>
2023-11-20 00:53:03 +0100

    fix: problems resolved including mouting for react-native-reanimated

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+
+PushNotificationIOS.addEventListener('localNotification', notification => console.log(notification));
 
 AppRegistry.registerComponent(appName, () => App);
